f9c5117d8be7ed4139a22375fee31ffcf13aef41
Rafael Lourenço <rafilourenco@gmail.com>
2023-09-29 11:14:53 +0100

    Adicionar comentarios do OlaMundo

--- Semana01/Argumentos/Program.cs
+++ Semana01/Argumentos/Program.cs
@@ -6,7 +6,8 @@ namespace Argumentos
     {
         static void Main(string[] args)
         {
-            for(int i = 0;i<args.Length;i++){
+            for (int i = 0; i < args.Length; i++)
+            {
                 Console.WriteLine(args[i]);
             }
         }
