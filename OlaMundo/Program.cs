64bcc7f94dba9ee2f4df7ae2890cfbe9830864f0
Rafael Lourenço <rafilourenco@gmail.com>
2023-09-29 11:33:35 +0100

    Adicionar variavel inteira

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -1,11 +1,17 @@
 ﻿using System;
-
+    /// <summary>
+    /// Este programa imprime a mensagem "Hello World!" na consola
+    /// </summary>
 namespace OlaMundo
 {
+    /// <summary>
+    /// Este método é responsavel por imprimir a mensagem
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
+            //Digita a mensagem
             Console.WriteLine("Hello World!");
         }
     }
