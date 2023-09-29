fa1cd47a8f37740fd28966648c8023b5b7fc1b0e
Rafael Lourenço <rafilourenco@gmail.com>
2023-09-29 11:53:20 +0100

    Adicionar reais

--- Semana01/TiposVarios/Program.cs
+++ Semana01/TiposVarios/Program.cs
@@ -8,6 +8,9 @@ namespace TiposVarios
         {
             int primeiro = 1;
             Console.WriteLine(primeiro);
+
+            double segundo = 1.55;
+            Console.WriteLine(segundo);
         }
     }
 }
