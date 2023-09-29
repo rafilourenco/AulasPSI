12be9eecb2b13fdad6b243ba977636cb78de5fa4
Rafael Lourenço <rafilourenco@gmail.com>
2023-09-29 11:02:24 +0100

    Adicionar comentarios

---
+++ Argumentos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Argumentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine(args[i]);
+            }
+        }
+    }
+}
