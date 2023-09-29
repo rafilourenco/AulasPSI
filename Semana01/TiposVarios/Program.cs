5bc604b4a6759fa461dc03756eec179a7cc882b8
Rafael Lourenço <rafilourenco@gmail.com>
2023-09-29 11:38:47 +0100

    Adicionar inteiro

---
+++ Semana01/TiposVarios/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int primeiro = 1;
+            Console.WriteLine(primeiro);
+        }
+    }
+}
