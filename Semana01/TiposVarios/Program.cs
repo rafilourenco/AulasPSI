d74d2bd5d3bb1c5ce6603556de9a9193f80d0e21
Rafael Lourenço <rafilourenco@gmail.com>
2023-09-29 11:58:58 +0100

    Adicionar char

--- Semana01/TiposVarios/Program.cs
+++ Semana01/TiposVarios/Program.cs
@@ -6,11 +6,17 @@ namespace TiposVarios
     {
         static void Main(string[] args)
         {
+            // Variavel Inteira
             int primeiro = 1;
             Console.WriteLine(primeiro);
 
+            // Variavel Real
             double segundo = 1.55;
             Console.WriteLine(segundo);
+
+            // Variavel Char
+            char terceiro = 'A';
+            Console.WriteLine(terceiro);
         }
     }
 }
