5025565e9362dd3fd3a430833b0f90ab6d33fb94
Rafael Lourenço <rafilourenco@gmail.com>
2023-10-02 11:10:37 +0100

    Adicionar variaveis tipo string

---
+++ Semana02/VariasStrings/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pais "Angola";
+            Console.WriteLine(pais);
+            string paisUni "Angola";
+            Console.WriteLine(paisUni);
+        }
+    }
+}
