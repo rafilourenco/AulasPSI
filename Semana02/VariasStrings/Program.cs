57107b2ef7fe0c2282ac10cc27eaac33302bd60e
Rafael Lourenço <rafilourenco@gmail.com>
2023-10-03 17:25:32 +0100

    Adicionar Incremento

--- Semana02/VariasStrings/Program.cs
+++ Semana02/VariasStrings/Program.cs
@@ -6,12 +6,31 @@ namespace VariasStrings
     {
         static void Main(string[] args)
         {
+            double dd = 0.12345;
+            int ii = 18;
+
             string pais = "Angola";
             Console.WriteLine(pais);
             string paisUni = "Angola\u00A9";
             Console.WriteLine(paisUni);
             string paisN = "Angola,\ngrande pais";
             Console.WriteLine(paisN);
+            string paisverb = @"Angola não tem símbolo de copyright \u00A9";
+            Console.WriteLine(paisverb);
+            string cont = "Angola" + " é " + "Grande";
+            Console.WriteLine(cont);
+            int x;
+            int y;
+            string soma = $"{x = 10} mais {y = 5} é igual a {x + y}";
+            Console.WriteLine(soma);
+            string s = String.Format("Bom dia {0}, Macacos {1}", "Angola", "Juntos Fortes");
+            Console.WriteLine(s);
+
+            string arren = $"x={dd:f2}";
+            Console.WriteLine(arren);
+            string decimal1 = String.Format($"‘{dd:p1}’");
+            Console.WriteLine(decimal1);
+            
         }
     }
 }
