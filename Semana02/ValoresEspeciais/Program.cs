00b4120e590453839f127c9266d49078addb10fb
Rafael Lourenço <rafilourenco@gmail.com>
2023-10-06 12:23:56 +0100

    Visualizar valores maximos e minimos

---
+++ Semana02/ValoresEspeciais/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte sx = sbyte.MaxValue;
+            double dx = double.MaxValue;
+            int ix = int.MaxValue;
+
+            sbyte sm = sbyte.MinValue;
+            double dm = double.MinValue;
+            int im = int.MinValue;
+
+            Console.WriteLine("Valores Maximos: ");
+            Console.WriteLine(sx);
+            Console.WriteLine(dx);
+            Console.WriteLine(ix);
+
+            Console.WriteLine("Valores Minimos: ");
+            Console.WriteLine(sm);
+            Console.WriteLine(dm);
+            Console.WriteLine(im);
+        }
+    }
+}
