57af8cae5b5c1b720a92766e410dea21dc48a62b
Rafael Lourenço <rafilourenco@gmail.com>
2023-10-06 11:01:37 +0100

    Volume e Area do Cilindro

--- Semana02/Cilindro/Program.cs
+++ Semana02/Cilindro/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Cilindro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double altura;
+            double raio;
+            double volume;
+            double superficie;
+
+            Console.WriteLine("Insira a altura do Cilindro: ");
+            altura = Convert.ToDouble ( Console.ReadLine() );
+            Console.WriteLine("Insira o raio do Cilindro: ");
+            raio = Convert.ToDouble ( Console.ReadLine() );
+
+            volume = 3.14 * (raio * raio) * altura;
+
+            Console.WriteLine("Aqui está o resultado do volume: ");
+            Console.WriteLine(volume);
+
+            superficie = 2 * 3.14 * raio * (raio + altura);
+            Console.WriteLine("E agora a superficie: ");
+            Console.WriteLine(superficie);
+
+
+        }
+    }
+}
