8685a8ab9708211ce581fa7129e5dd13ce6d07ba
Rafael Lourenço <rafilourenco@gmail.com>
2023-10-11 16:04:28 +0100

    Exercicio Piramide

---
+++ Semana03/Piramide/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Piramide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int resposta;
+            string asterisco = "*";
+            Console.WriteLine("Introduza um número!");
+            resposta = Convert.ToInt32(Console.ReadLine());
+
+
+            if(resposta > 1){
+                for(int i = 0; i < resposta; i++){
+                    for(int x = resposta - i; x>0;x--){
+                        Console.Write(" ");
+                    }
+                    for(int y = 0; y< resposta - i;y++){
+                        Console.Write(asterisco);
+                    }
+                    Console.WriteLine();
+                }
+            }else{
+                Console.WriteLine("Tem de introduzir um número maior que 1!");
+            }
+            
+        }
+    }
+}
