d10c3d6a97b69273cfbdaf5058908a0793e2d9b0
Silva <rafilourenco@gmail.com>
2023-11-03 12:20:55 +0000

    Fim do Exercicio

--- Semana05/ValorEReferencia/Program.cs
+++ Semana05/ValorEReferencia/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ValorEReferencia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = a;
+
+            int[] array1 = new int[] {1,2,3};
+            int[] array2 = array1;
+
+            
+            
+            for(int i = 0; i < array1.Length;i++){
+                Console.WriteLine(array1[i]);
+            }
+            array1[1] = a;
+            Console.WriteLine(" ");
+            for(int i = 0; i < array2.Length;i++){
+                Console.WriteLine(array2[i]);
+            }
+            array2 = new int[] {6,7,8};
+            Console.WriteLine(" ");
+            for(int i = 0; i < array1.Length;i++){
+                Console.WriteLine(array1[i]);
+            }
+
+        }
+    }
+}
