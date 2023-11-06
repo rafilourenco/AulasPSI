123beacb34e71466ee7ae9280fbfea5af6f96860
Silva <rafilourenco@gmail.com>
2023-11-06 13:10:23 +0000

    Exercicio StopWatch

---
+++ Semana05/Chronos/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Chronos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            // Declarar e instanciar dois cronómetros
+            Stopwatch cron1 = new Stopwatch();
+            Stopwatch cron2 = new Stopwatch();
+
+            // Iniciar contagem do tempo em cron1
+            cron1.Start();
+
+            // Pausar o programa durante 1 segundo:
+            Thread.Sleep(1000);
+
+            // Iniciar contagem do tempo em cron2
+            cron2.Start();
+
+            // Pausar o programa durante mais 1 segundo
+            Thread.Sleep(1000);
+
+            //Parar contagem do tempo nos dois cronómetros
+            cron1.Stop();
+            cron2.Stop();
+
+            // Mostrar no ecrã tempo decorrido em cada um dos cronómetros, em milissegundos
+            Console.WriteLine("Cronometro 1: "+cron1.ElapsedMilliseconds);
+            Console.WriteLine("Cronometro 2: "+cron2.ElapsedMilliseconds);
+
+            /*
+            1: cron1, cron2 de stopwatch
+            2: Sim
+            3: Tempo Decorrido
+            
+
+
+
+
+            */
+        }
+    }
+}
---
+++ Semana05/EX4.md
@@ -0,0 +1,20 @@
+NPC-
+        Estado: Vida, Dano, Velocidade
+        Ações: Morrer(), Falar(), Fugir()
+        Instâncias: Monstro, Soldado, Civil
+Automóvel-
+        Estado: Cor, Quilometragem, Combustivel
+        Ações: EncherDeposito(),Limpar(), Vender()
+        Instâncias: Volkswagen Beatle, Seat Ibiza
+Asteroide-
+        Estado: Tamanho, Cor, Forma
+        Ações: Explodir(), Derivar()
+        Instâncias: Pequeno, Grande
+Nave-
+        Estado: Cor, Tamanho, Velocidade
+        Ações: Disparar(), Mover(), Teleportar()
+        Instâncias: Nave de Batalha, Nave de Comercio, Nave de Exploração
+Cronómetro-
+        Estado: Preço, Cor, Precisão
+        Ações: Temporizador(), Despertador(), Cronometro()
+        Instâncias: Corrida, Digital
