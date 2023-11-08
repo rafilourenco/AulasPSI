9196c2305cf6b34f62731aede76910fa1bfba958
Silva <rafilourenco@gmail.com>
2023-11-08 13:01:29 +0000

    Exercicio 7: Class Random

---
+++ Semana05/RandomTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RandomTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int soma = 0;
+            Random rnd = new Random();
+
+            Console.WriteLine("Introduza o número de dados a lançar: ");
+            n = Convert.ToInt32 (Console.ReadLine());
+
+            
+            for (int i = 1; i <= n; i++){
+               int aux = rnd.Next(1,6);
+               Console.WriteLine($"Valor do dado {i}: {aux}");
+               soma += aux;
+            }
+
+
+            Console.WriteLine($"Valor da soma: {soma}");
+
+                
+         }
+    }
+}
