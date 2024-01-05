0eb79964fa3fc414a96624206ecd04bcc837c116
Silva <rafilourenco@gmail.com>
2024-01-05 11:05:59 +0000

    Fim do Exercicio 1

---
+++ Semana09/Escrever01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace Escrever01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "ficheiro.txt");
+            int i = 0;
+            string input;
+            string[] inputs = new string[10];
+
+            do{
+                
+                input = Console.ReadLine();
+                if(input != ""){
+                    inputs[i] = input;
+                }
+                i++;
+            }while(input != "");
+
+            File.WriteAllLines(path, inputs);
+        }
+    }
+}
