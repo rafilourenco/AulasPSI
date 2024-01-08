116779a3b815ef5afd65a4ded3d0fe6c98f66c41
Silva <rafilourenco@gmail.com>
2024-01-08 10:58:55 +0000

    Começo do Exercicio 3

---
+++ Semana09/TesteErros/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TesteErros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insere um número inteiro: ");
+            int i = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Número inserido: {i}");
+
+        }
+    }
+}
