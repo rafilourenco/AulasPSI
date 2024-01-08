ef1585700cb80821850f231a13b2f79ec2a8805a
Silva <rafilourenco@gmail.com>
2024-01-08 11:03:41 +0000

    Fim do Exercicio 4

--- Semana09/TesteErros/Program.cs
+++ Semana09/TesteErros/Program.cs
@@ -5,10 +5,17 @@ namespace TesteErros
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.Write("Insere um número inteiro: ");
-            int i = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Número inserido: {i}");
+        {   
+
+            try{
+                Console.Write("Insere um número inteiro: ");
+                int i = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Número inserido: {i}");
+            }
+            catch (Exception e){
+                Console.WriteLine("Ocorreu o seguinte problema: " + e.Message);
+            }
+            
 
         }
     }
